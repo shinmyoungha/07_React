8a141e88a216e19c9f6313b3f877a6612429d452
shinmyoungha <audgk0309@naver.com>
2025-05-30 11:10:32 +0900

    복구

--- board-project-admin/src/components/Restore.jsx
+++ board-project-admin/src/components/Restore.jsx
@@ -1,21 +1,162 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { axiosApi } from "../api/axiosAPI";
 
 export default function Restore() {
-  return (
-    <div className="menu-box">
-      <section className="section-border">
-        <h2>탈퇴 회원 복구</h2>
+  const [withdrawnMembers, setWithdrawnMembers] = useState(null); // 탈퇴 회원 목록
+  const [deleteBoards, setDeleteBoards] = useState(null); // 삭제 게시글 목록
+  const [isLoading, setIsLoading] = useState(true); // 로딩 상태
 
-        <h3>탈퇴한 회원 목록</h3>
+  // 탈퇴한 회원 목록 조회용 함수
+  const getWithdrawnMemberList = async () => {
+    try {
+      const resp = await axiosApi.get("/admin/withdrawnMemberList");
 
-      </section>
+      console.log(resp.data);
+      if (resp.status === 200) {
+        setWithdrawnMembers(resp.data);
+      }
+    } catch (error) {
+      console.log("탈퇴 회원 목록 조회 중 에러 발생 : ", error);
+    }
+  };
 
-      <section className="section-border">
-        <h2>삭제 게시글 복구</h2>
+  // 탈퇴한 회원 복구 요청 함수
+  const restoreMember = async (member) => {
+    if (
+      window.confirm(member.memberNickname + "님을 탈퇴 복구 시키겠습니까?")
+    ) {
+      try {
+        const resp = await axiosApi.put("/admin/restoreMember", {
+          memberNo: member.memberNo,
+        });
 
-        <h3>삭제된 게시글 목록</h3>
+        if (resp.status === 200) {
+          alert("복구 되었습니다!");
+          getWithdrawnMemberList(); // 꼭 필요함!
+          // 복구된 데이터 확인 (업데이트된 내역을 확인하기 위해서)
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
-      </section>
-    </div>
-  );
+  // 삭제된 게시글 목록 조회용 함수
+  const getDeleteBoardList = async () => {
+    try {
+      const resp = await axiosApi.get("/admin/deleteBoardList");
+      console.log(resp.data);
+
+      if (resp.status === 200) {
+        setDeleteBoards(resp.data);
+      }
+    } catch (error) {
+      console.log("삭제된 게시글 목록 조회 중 에러 발생 : ", error);
+    }
+  };
+
+  // 삭제된 게시글 복구 요청 함수
+  const restoreBoard = async (board) => {
+    if (window.confirm(board.boardNo + "번 게시글을 복구 시키겠습니까?")) {
+      try {
+        const resp = await axiosApi.put("/admin/restoreBoard", {
+          boardNo: board.boardNo,
+        });
+
+        if (resp.status === 200) {
+          alert("복구 되었습니다!");
+          getDeleteBoardList(); // 꼭 필요함!
+          // 복구된 데이터 확인 (업데이트된 내역을 확인하기 위해서)
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  // Restore 컴포넌트가 처음 렌더링 될 때 실행
+  useEffect(() => {
+    getWithdrawnMemberList();
+    getDeleteBoardList();
+  }, []);
+
+  // withdrawnMembers, deleteBoards 상태가 변경될 때 실행(isLoading 값 변경)
+  useEffect(() => {
+    if (withdrawnMembers != null && deleteBoards != null) {
+      setIsLoading(false);
+    }
+  }, [withdrawnMembers, deleteBoards]);
+
+  //상태 값 null    -> 서버에게서 응답받기 전
+  //상태 값 [ ]      -> 서버에게 응답 받음(조회된 데이터 없음)
+  //상태 값 [요소] -> 서버에게 응답 받음(조회된 데이터 있음)
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  } else {
+    return (
+      <div className="menu-box">
+        <RestoreMember
+          withdrawnMembers={withdrawnMembers}
+          restoreMember={restoreMember}
+        />
+
+        <RestoreBoard deleteBoards={deleteBoards} restoreBoard={restoreBoard} />
+      </div>
+    );
+  }
 }
+
+const RestoreMember = ({ withdrawnMembers, restoreMember }) => {
+  return (
+    <section className="section-border">
+      <h2>탈퇴 회원 복구</h2>
+
+      <h3>탈퇴한 회원 목록</h3>
+
+      {withdrawnMembers.length === 0 ? (
+        <p>탈퇴한 회원이 없습니다</p>
+      ) : (
+        withdrawnMembers.map((member, index) => {
+          return (
+            <ul className="ul-board" key={index}>
+              <li>회원 번호 : {member.memberNo}</li>
+              <li>회원 이메일 : {member.memberEmail}</li>
+              <li>회원 닉네임 : {member.memberNickname}</li>
+              <button
+                className="restoreBtn"
+                onClick={() => restoreMember(member)}
+              >
+                복구
+              </button>
+            </ul>
+          );
+        })
+      )}
+    </section>
+  );
+};
+
+const RestoreBoard = ({ deleteBoards, restoreBoard }) => {
+  return (
+    <section className="section-border">
+      <h2>삭제 게시글 복구</h2>
+
+      <h3>삭제된 게시글 목록</h3>
+      {deleteBoards.length === 0 ? (
+        <p>삭제된 게시글이 없습니다</p>
+      ) : (
+        deleteBoards.map((board, index) => (
+          <ul className="ul-board" key={index}>
+            <li>게시글 번호 : {board.boardNo}</li>
+            <li>게시글 카테고리명 : {board.boardName}</li>
+            <li>게시글 제목 : {board.boardTitle}</li>
+            <li>작성자 닉네임 : {board.memberNickname}</li>
+            <button className="restoreBtn" onClick={() => restoreBoard(board)}>
+              복구
+            </button>
+          </ul>
+        ))
+      )}
+    </section>
+  );
+};
--- board-project-admin/src/components/Statistics.jsx
+++ board-project-admin/src/components/Statistics.jsx
@@ -5,52 +5,72 @@ export default function Statistics() {
   const [readCountData, setReadCountData] = useState(null);
   const [likeCountData, setLikeCountData] = useState(null);
   const [commentCountData, setCommentCountData] = useState(null);
-  const [isLoding, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
+  const [newMembers, setNewMembers] = useState(null);
 
-  // 비동기 요청
+  // 7일내 신규회원 리스트 조회
+  const fetchNewMembers = async () => {
+    try {
+      const response = await axiosApi.get("/admin/newMember");
+
+      if (response.status === 200) {
+        setNewMembers(response.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   // 최대 조회 수 게시글 조회
   const getMaxReadCount = async () => {
     try {
       const resp = await axiosApi.get("/admin/maxReadCount");
       console.log(resp.data);
 
-      if (resp.status === 200) setReadCountData(resp.data);
+      if (resp.status === 200) {
+        setReadCountData(resp.data);
+      }
     } catch (error) {
       console.log("최대 조회 수 게시글 조회 중 예외 발생 : ", error);
     }
   };
 
-  // 최대 좋아요 수 게시글 조회
+  // 최대 좋아요수 게시글 조회
   const getMaxLikeCount = async () => {
     try {
       const resp = await axiosApi.get("/admin/maxLikeCount");
       console.log(resp.data);
 
-      if (resp.status === 200) setLikeCountData(resp.data);
+      if (resp.status === 200) {
+        setLikeCountData(resp.data);
+      }
     } catch (error) {
-      console.log("최대 좋아요 수 게시글 조회 중 예외 발생 : ", error);
+      console.log("최대 좋아요수 게시글 조회 중 예외 발생 : ", error);
     }
   };
 
-  // 최대 댓글 수 게시글 조회
+  // 최대 댓글수 게시글 조회
   const getMaxCommentCount = async () => {
     try {
       const resp = await axiosApi.get("/admin/maxCommentCount");
       console.log(resp.data);
 
-      if (resp.status === 200) setCommentCountData(resp.data);
+      if (resp.status === 200) {
+        setCommentCountData(resp.data);
+      }
     } catch (error) {
-      console.log("최대 댓글 수 게시글 조회 중 예외 발생 : ", error);
+      console.log("최대 댓글수 게시글 조회 중 예외 발생 : ", error);
     }
   };
 
   // 컴포넌트가 처음 마운트 될 때 딱 1번만 실행
   // -> Statistics 컴포넌트가 화면에 마운트 될 때 서버로 세가지 데이터 요청, 응답받아야함.
   useEffect(() => {
+    fetchNewMembers();
     getMaxReadCount();
     getMaxLikeCount();
     getMaxCommentCount();
-  }, []); // 의존성 배열 비어있기 때문에 1번만 실행
+  }, []); // 의존성 배열이 비어있기 때문에 1번만 실행
 
   // readCountData, likeCountData, commentCountData에 변화가 감지될 때
   // -> isLoading 상태값을 false로 변경하기
@@ -58,53 +78,85 @@ export default function Statistics() {
     if (
       readCountData != null &&
       likeCountData != null &&
-      commentCountData != null
+      commentCountData != null &&
+      newMembers != null
     ) {
       setIsLoading(false);
     }
-  }, [readCountData, likeCountData, commentCountData]);
+  }, [readCountData, likeCountData, commentCountData, newMembers]);
 
-  if (isLoding) {
+  if (isLoading) {
     return <h1>Loading...</h1>;
   } else {
     return (
       <div>
-        <h2>신규 가입 회원 </h2>
-        <h3>[7일 이내 가입 회원]</h3>
-        <table>
-          <th>회원번호 이메일 닉네임 가입일</th>
-        </table>
+        {/* 7일 이내 가입한 신규 회원 조회 */}
+        <NewMembers data={newMembers} />
+
         <section className="statistics-section">
           <h2>가장 조회수 많은 게시글</h2>
-          <p>게시글 종류 : {readCountData.boardName}</p>
+          <p>게시판 종류 : {readCountData.boardName}</p>
           <p>
-            게시글 번호/제목 : No.{readCountData.boardNo}/{" "}
+            게시글 번호/제목 : No.{readCountData.boardNo} /{" "}
             {readCountData.boardTitle}
           </p>
           <p>게시글 조회 수 : {readCountData.readCount}</p>
           <p>작성자 닉네임 : {readCountData.memberNickname}</p>
         </section>
+
         <section className="statistics-section">
           <h2>가장 좋아요 많은 게시글</h2>
-          <p>게시글 종류 : {likeCountData.boardName}</p>
+          <p>게시판 종류 : {likeCountData.boardName}</p>
           <p>
-            게시글 번호/제목 : No.{likeCountData.boardNo}/{" "}
+            게시글 번호/제목 : No.{likeCountData.boardNo} /{" "}
             {likeCountData.boardTitle}
           </p>
-          <p>게시글 조회 수 : {likeCountData.readCount}</p>
+          <p>게시글 좋아요 수 : {likeCountData.likeCount}</p>
           <p>작성자 닉네임 : {likeCountData.memberNickname}</p>
         </section>
+
         <section className="statistics-section">
           <h2>가장 댓글 많은 게시글</h2>
-          <p>게시글 종류 : {commentCountData.boardName}</p>
+          <p>게시판 종류 : {commentCountData.boardName}</p>
           <p>
-            게시글 번호/제목 : No.{commentCountData.boardNo}/{" "}
+            게시글 번호/제목 : No.{commentCountData.boardNo} /{" "}
             {commentCountData.boardTitle}
           </p>
-          <p>게시글 조회 수 : {commentCountData.readCount}</p>
+          <p>게시글 댓글 수 : {commentCountData.commentCount}</p>
           <p>작성자 닉네임 : {commentCountData.memberNickname}</p>
         </section>
       </div>
     );
   }
 }
+
+// 신규 회원 조회
+const NewMembers = ({ data }) => {
+  return (
+    <div className="new-members">
+      <h2>신규 가입 회원 ({data.length}명)</h2>
+      <h3>[ 7일 이내 가입 회원 ]</h3>
+      <table border={1}>
+        <thead>
+          <tr>
+            <th>회원번호</th>
+            <th>이메일</th>
+            <th>닉네임</th>
+            <th>가입일</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {data.map((member) => (
+            <tr key={member.memberNo}>
+              <td>{member.memberNo}</td>
+              <td>{member.memberEmail}</td>
+              <td>{member.memberNickname}</td>
+              <td>{member.enrollDate}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
