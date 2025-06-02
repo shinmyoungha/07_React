fff6b6cfd69712378956440d1a9f787332f938e2
shinmyoungha <audgk0309@naver.com>
2025-06-02 12:51:49 +0900

    관리자 권한 부여

--- board-project-admin/src/components/Manager.jsx
+++ board-project-admin/src/components/Manager.jsx
@@ -1,6 +1,61 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { axiosApi } from "../api/axiosAPI";
 
 export default function Manager() {
+  // 이메일, 닉네임 전화번호
+  // 객체 하나로 상태 관리하는 방식
+  const [form, setForm] = useState({
+    email: "",
+    nickname: "",
+    tel: "",
+  });
+
+  // 객체형태 상태 변경 함수
+  const handleChange = (e) => {
+    const { id, value } = e.target; // 대상의 id 속성 값, value 값을 꺼내옴
+    setForm((prev) => ({
+      ...prev,
+      [id]: value,
+    }));
+  };
+
+  // 관리자 계정 발급
+  async function createAdminAccount() {
+    const { email, nickname, tel } = form; // form 상태 안에 있는 값들 하나씩 꺼내오기
+
+    if (email.length === 0 || nickname.length === 0 || tel.length === 0) {
+      alert("모든 필드를 입력해주세요!");
+      return;
+    }
+
+    try {
+      const response = await axiosApi.post("/admin/createAdminAccount", {
+        memberEmail: email,
+        memberNickname: nickname,
+        memberTel: tel,
+      });
+
+      if (response.status === 201) {
+        const result = response.data; // 서버에서 응답해준 데이터 (body)
+        alert(
+          `발급된 비밀번호는 ${result} 입니다. 다시 확인할 수 없으니 저장해주시기 바랍니다.`
+        );
+        console.log(result);
+      }
+
+      // 입력필드 초기화
+      setForm({
+        email: "",
+        nickname: "",
+        tel: "",
+      });
+    } catch (err) {
+      alert(err.response.data);
+      // 409일 때, 500일 때 응답받은 body 내용이 반영되어 alert 출력할 수 있게끔 함.
+    }
+  }
+
   return (
     <>
       <div className="manager-div">
@@ -14,6 +69,8 @@ export default function Manager() {
                   id="email"
                   type="email"
                   placeholder="ex) admin2@kh.or.kr"
+                  value={form.email}
+                  onChange={handleChange}
                 />
               </td>
             </tr>
@@ -24,6 +81,8 @@ export default function Manager() {
                   id="nickname"
                   type="text"
                   placeholder="ex) 관리자2"
+                  value={form.nickname}
+                  onChange={handleChange}
                 />
               </td>
             </tr>
@@ -34,11 +93,13 @@ export default function Manager() {
                   id="tel"
                   type="text"
                   placeholder="ex) 01012341234"
+                  value={form.tel}
+                  onChange={handleChange}
                 />
               </td>
             </tr>
           </table>
-          <button className="issueBtn">
+          <button className="issueBtn" onClick={createAdminAccount}>
             발급
           </button>
         </section>
@@ -51,7 +112,6 @@ export default function Manager() {
               <th>이메일</th>
               <th>관리자명</th>
             </tr>
-          
           </table>
         </section>
       </div>
